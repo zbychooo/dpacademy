99ab271ceafa569cd5a9cb95d5e8adb42af1fa86
zbychooo <zbyszko@protonmail.com>
2016-08-18 22:01:27 +0200

    Test update

--- src/main/java/edu/dpacademy/DPAcademy.java
+++ src/main/java/edu/dpacademy/DPAcademy.java
@@ -3,6 +3,6 @@ package edu.dpacademy;
 public class DPAcademy {
 
     public static void main( String[] args ) {
-        System.out.println( "Hello World!" );
+        System.out.println( "!!!!!!!!!!!!!!! Hello World!" );
     }
 }
---
+++ src/test/java/edu/dpacademy/builder/PreferenceBuilderTest.java
@@ -0,0 +1,35 @@
+package edu.dpacademy.builder;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class PreferenceBuilderTest {
+
+    @Test
+    public void createPreference() throws Exception {
+
+        PreferenceBuilder builder = new PreferenceBuilder();
+
+        int mockId = 1;
+        String categoryName = "category-name";
+        String internalName = "internal-name";
+        String displayName = "display-name";
+        Long value = Long.valueOf(20);
+        Preference preference = builder
+                .setId(mockId)
+                .setCategoryName(categoryName)
+                .setInternalName(internalName)
+                .setDisplayName(displayName)
+                .setValue(value)
+                .createPreference();
+
+        assertEquals(mockId, preference.getId());
+        assertEquals(categoryName, preference.getCategoryName());
+        assertEquals(internalName, preference.getInternalName());
+        assertEquals(displayName, preference.getDisplayName());
+        assertEquals(value, preference.getValue());
+
+    }
+
+}
