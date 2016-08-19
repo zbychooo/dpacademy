a721d504cca0ddd08b9b294c80b68cea89d0e78a
zbychooo <zbyszko@protonmail.com>
2016-08-19 13:31:06 +0200

    Factory pattern tests

--- src/test/java/edu/dpacademy/factory/AttributeDefinitionFactoryTest.java
+++ src/test/java/edu/dpacademy/factory/AttributeDefinitionFactoryTest.java
@@ -1,8 +1,8 @@
 package edu.dpacademy.factory;
 
-import edu.dpacademy.factory.definition.AttributeDefinition;
-import edu.dpacademy.factory.definition.BooleanAttributeDefinition;
+import edu.dpacademy.factory.definition.*;
 import edu.dpacademy.factory.util.AttrEnum;
+import edu.dpacademy.factory.util.UnitEnum;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -26,7 +26,60 @@ public class AttributeDefinitionFactoryTest {
         assertEquals(displayName, ((BooleanAttributeDefinition) attributeDefinition).getDisplayName());
         assertEquals(internalName, ((BooleanAttributeDefinition) attributeDefinition).getInternalName());
         assertEquals(value, ((BooleanAttributeDefinition) attributeDefinition).getValue());
-
     }
 
+    @Test
+    public void newAttributeDefinition_Number() throws Exception {
+
+        Long id = 1L;
+        String displayName="Vodka bottle";
+        String internalName="com.bottle.vodka";
+        Double value = 0.7d;
+
+        AttributeDefinition attributeDefinition = AttributeDefinitionFactory.
+                newAttributeDefinition(AttrEnum.NUMBER, id, internalName, displayName, value, null);
+
+        assertTrue(attributeDefinition instanceof NumberAttributeDefinition);
+        assertEquals(id, ((NumberAttributeDefinition) attributeDefinition).getId());
+        assertEquals(displayName, ((NumberAttributeDefinition) attributeDefinition).getDisplayName());
+        assertEquals(internalName, ((NumberAttributeDefinition) attributeDefinition).getInternalName());
+        assertEquals(value, ((NumberAttributeDefinition) attributeDefinition).getValue());
+    }
+
+    @Test
+    public void newAttributeDefinition_String() throws Exception {
+
+        Long id = 1L;
+        String displayName="The name of all names";
+        String internalName="com.text.loreum";
+        String value = "Loreum ipsum";
+
+        AttributeDefinition attributeDefinition = AttributeDefinitionFactory.
+                newAttributeDefinition(AttrEnum.STRING, id, internalName, displayName, value, null);
+
+        assertTrue(attributeDefinition instanceof StringAttributeDefinition);
+        assertEquals(id, ((StringAttributeDefinition) attributeDefinition).getId());
+        assertEquals(displayName, ((StringAttributeDefinition) attributeDefinition).getDisplayName());
+        assertEquals(internalName, ((StringAttributeDefinition) attributeDefinition).getInternalName());
+        assertEquals(value, ((StringAttributeDefinition) attributeDefinition).getValue());
+    }
+
+    @Test
+    public void newAttributeDefinition_Unit() throws Exception {
+
+        Long id = 1L;
+        String displayName="The distance in meters";
+        String internalName="com.distance.unit";
+        Double value = 1.83;
+        UnitEnum unitEnum = UnitEnum.LENGTH;
+
+        AttributeDefinition attributeDefinition = AttributeDefinitionFactory.
+                newAttributeDefinition(AttrEnum.UNIT_NUMBER, id, internalName, displayName, value, unitEnum);
+
+        assertTrue(attributeDefinition instanceof UnitNumberAttributeDefinition);
+        assertEquals(id, ((UnitNumberAttributeDefinition) attributeDefinition).getId());
+        assertEquals(displayName, ((UnitNumberAttributeDefinition) attributeDefinition).getDisplayName());
+        assertEquals(internalName, ((UnitNumberAttributeDefinition) attributeDefinition).getInternalName());
+        assertEquals(value, ((UnitNumberAttributeDefinition) attributeDefinition).getValue());
+    }
 }
